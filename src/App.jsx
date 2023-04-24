26ebe20cb56fbdeefa925b836bade672c18566ce
Treasure Ajefu <treasureajefu@gmail.com>
2023-04-24 14:05:24 +0100

    changing themes functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState("purple");
+
+  function changeTheme() {
+    if (theme === "purple") {
+      setTheme("green");
+    } else if (theme === "green") {
+      setTheme("brown");
+    } else {
+      setTheme("purple");
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main
+        className={`${
+          theme === "purple"
+            ? "bg-purple-700"
+            : theme === "green"
+            ? "bg-green-700"
+            : theme === "brown"
+            ? "bg-red-500"
+            : ""
+        }`}
+      >
+        <h1>Theme: {theme}</h1>
+        <button onClick={changeTheme}>Change Theme</button>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
